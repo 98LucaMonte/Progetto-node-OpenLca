bda29cde8eeeae99ecb12448f2726a413a063b45
Luca <luca.monteleone98@gmail.com>
2023-11-17 18:30:20 +0100

    Cambiamenti parte grafica dopo il calcolo

--- gdt server/Tesi/public/js/templates/header-view.js
+++ gdt server/Tesi/public/js/templates/header-view.js
@@ -26,4 +26,23 @@ function creaViewHeader(){
     `;
 }
 
-export {creaViewHeader};
+function creaViewHeaderRisultati(){ 
+  return `
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling"
+          aria-controls="offcanvasScrolling">
+          <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-info-circle"
+            viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            <path
+              d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+          </svg>
+        </button>
+        <h3 style="margin-right: 80%;">Titolo Pagina</h3>
+      </div>
+    </nav>
+  `;
+}
+
+export {creaViewHeader,creaViewHeaderRisultati};
