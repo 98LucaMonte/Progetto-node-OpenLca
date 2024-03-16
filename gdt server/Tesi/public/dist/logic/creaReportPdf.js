4b869beda95a5331f54643752cae8d6765c79483
Luca <luca.monteleone98@gmail.com>
2024-03-16 15:44:31 +0100

    Inserimento report pdf

--- gdt server/Tesi/public/dist/logic/creaReportPdf.js
+++ gdt server/Tesi/public/dist/logic/creaReportPdf.js
@@ -7,22 +7,50 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { ApiFlowResults } from "../backend/apiFlowResult";
-import { ApiImpactResults } from "../backend/apiImpactResults";
+import { ApiFlowResults } from "../backend/apiFlowResult.js";
+import { ApiImpactResults } from "../backend/apiImpactResults.js";
+import { ApiResultQueries } from "../backend/apiResultQueries.js";
 //@ts-ignore
 const { jsPDF } = window.jspdf;
 const apiFlowResults = new ApiFlowResults();
 const apiImpactResults = new ApiImpactResults();
+const apiResultQueries = new ApiResultQueries();
 // Funzione per creare un PDF
 export function creaPDF(idCalcolo) {
     return __awaiter(this, void 0, void 0, function* () {
         // Crea un nuovo documento PDF
         const doc = new jsPDF();
-        const listaInvetory = yield apiFlowResults.getInventoryResult(idCalcolo);
+        const datiCalcolo = yield apiResultQueries.getRichiestaFinale(idCalcolo);
+        const nomeProductSystem = "" + datiCalcolo.techFlow.provider.name;
+        const listaInventory = yield apiFlowResults.getInventoryResult(idCalcolo);
+        console.log(listaInventory);
         const listaImpactCategory = yield apiImpactResults.getTotalImpacts(idCalcolo);
+        let indice = 0;
+        let datiTabellaImpactCategory = listaImpactCategory.map((elemento) => {
+            const amountFormatted = parseFloat("" + elemento.amount).toFixed(7);
+            indice++;
+            return {
+                'Num': "" + indice,
+                'Nome': elemento.impactCategory.name,
+                'Categoria': elemento.impactCategory.category,
+                'Quantità': amountFormatted + " " + elemento.impactCategory.refUnit
+            };
+        });
+        let headers = ['Num', 'Nome', 'Categoria', 'Quantità'];
+        let config = {
+            "printHeaders": true,
+            "autoSize": true,
+            "fontSize": 10
+        };
+        // Imposta la dimensione del testo per le celle della tabella
+        doc.setFontSize(12);
+        //Titolo del PDF
+        doc.text("Risultati del calcolo del sistema di prodotto: " + nomeProductSystem, 10, 10);
         // Aggiungi testo al documento
-        doc.text("Hello World!", 10, 10);
+        doc.text("Tabella che mostra le categorie di impatto del sistema di prodotto appena calcolato", 10, 20);
+        doc.table(10, 30, datiTabellaImpactCategory, headers, config);
+        doc.addPage();
         // Salva il documento come file PDF
-        doc.save("example.pdf");
+        doc.save("Report_" + nomeProductSystem + ".pdf");
     });
 }
--- gdt server/Tesi/public/ts/logic/creaReportPdf.ts
+++ gdt server/Tesi/public/ts/logic/creaReportPdf.ts
@@ -1,25 +1,55 @@
-import { ApiFlowResults } from "../backend/apiFlowResult";
-import { ApiImpactResults } from "../backend/apiImpactResults";
-import { EnviFlowData, ImpactCategoryData } from "../model/types";
+import { ApiFlowResults } from "../backend/apiFlowResult.js";
+import { ApiImpactResults } from "../backend/apiImpactResults.js";
+import { ApiResultQueries } from "../backend/apiResultQueries.js";
+import { EnviFlowData, ImpactCategoryData } from "../model/types.js";
 
 //@ts-ignore
 const { jsPDF } = window.jspdf;
 const apiFlowResults = new ApiFlowResults();
 const apiImpactResults = new ApiImpactResults();
-
+const apiResultQueries = new ApiResultQueries();
 
 // Funzione per creare un PDF
 export async function creaPDF(idCalcolo: string): Promise<void> {
   // Crea un nuovo documento PDF
   const doc = new jsPDF();
-
-  const listaInvetory:EnviFlowData[] = await apiFlowResults.getInventoryResult(idCalcolo);
+  const datiCalcolo = await apiResultQueries.getRichiestaFinale(idCalcolo);
+  const nomeProductSystem:string = ""+datiCalcolo.techFlow.provider.name;
+  const listaInventory:EnviFlowData[] = await apiFlowResults.getInventoryResult(idCalcolo);
+  console.log(listaInventory);
   const listaImpactCategory:ImpactCategoryData[] = await apiImpactResults.getTotalImpacts(idCalcolo);
+  let indice:number = 0
+  let datiTabellaImpactCategory = listaImpactCategory.map((elemento)=>{
+    const amountFormatted = parseFloat(""+elemento.amount).toFixed(7);
+    indice++;
+    return {
+      'Num.': ""+indice,
+      'Nome': elemento.impactCategory.name,
+      'Categoria': elemento.impactCategory.category,
+      'Quantità': amountFormatted + " " + elemento.impactCategory.refUnit 
+    };
+  })
+  let headers: string [] = ['Num.', 'Nome', 'Categoria', 'Quantità'];
 
+  let config:object ={
+    "printHeaders":true,
+    "autoSize":true,
+    "fontSize":10
+  }
+  
+  // Imposta la dimensione del testo per le celle della tabella
+  doc.setFontSize(12);
+  
+  //Titolo del PDF
+  doc.text("Risultati del calcolo del sistema di prodotto: "+nomeProductSystem, 10, 10);
 
   // Aggiungi testo al documento
-  doc.text("Hello World!", 10, 10);
+  doc.text("Tabella che mostra le categorie di impatto del sistema di prodotto appena calcolato", 10, 20);
+
+  doc.table(10,30,datiTabellaImpactCategory,headers,config);
+
+  doc.addPage();
 
   // Salva il documento come file PDF
-  doc.save("example.pdf");
+  doc.save("Report_"+nomeProductSystem+".pdf");
 }
